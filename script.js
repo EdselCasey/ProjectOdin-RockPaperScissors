b568a8eed44007a0b20e61491038539b205f2da3
EdselCasey <edselcasey@yahoo.com>
2023-05-10 07:28:49 -0400

    built rock paper scissors game
    not finished has an error where the score is not being updated at the start of the game and after a drew. It does stop the game after someone gets to 3.

---
+++ script.js
@@ -0,0 +1,97 @@
+function getComputerChoice() {
+    const computerOptions = ["rock","paper","scissors"]
+    const randNum = Math.floor(Math.random() * 3)
+    return computerOptions[randNum]
+}
+
+function getPlayerChoice(){
+    let correctChoice = false;
+    let playerSelection;
+do {
+    let userInput = prompt("Choose Rock, Paper or Scissors...");
+    let userInputLowercase = userInput.toLowerCase();
+    if (userInputLowercase === "rock" || userInputLowercase === "paper" || userInputLowercase === "scissors") {
+        playerSelection = userInputLowercase
+        correctChoice = true
+    }else{
+        console.log("Invalid choice: Choose rock, paper or scissors!")
+    }
+}while (!correctChoice)
+return playerSelection;
+}
+
+    
+
+function gameRound() {
+    let playerWin = false;
+    let playerSelection = getPlayerChoice()
+    let computerSelection = getComputerChoice();
+    console.log(playerSelection)
+    console.log(computerSelection)
+
+    if (playerSelection == computerSelection){
+        playerWin = null
+        console.log("It's a Draw..");
+        return playerWin;
+    }
+    else if (playerSelection == "rock") {
+        if(computerSelection == "paper"){
+            playerWin = false
+            console.log("You lose!")
+            return playerWin;
+        }else if(computerSelection == "scissors"){
+            playerWin = true;
+            console.log("You win!")
+            return playerWin;
+        }
+    }
+    else if(playerSelection == "paper") {
+        if(computerSelection == "scissors"){
+            playerWin = false
+            console.log("You lose!")
+            return playerWin;
+        }else if(computerSelection == "rock"){
+            playerWin = true;
+            console.log("You win!")
+            return playerWin;
+        }
+    }
+    else if(playerSelection == "scissors") {
+        if(computerSelection == "rock"){
+            playerWin = false
+            console.log("You lose!")
+            return playerWin;
+        }else if(computerSelection == "paper"){
+            playerWin = true;
+            console.log("You win!")
+            return playerWin;
+        }
+    }
+}
+
+
+function Game(){
+let playerScore = 0;
+let computerScore = 0;
+do {
+if(gameRound()) {
+    playerScore++
+    console.log(`Player Score: ${playerScore}`)
+    console.log(`Computer Score: ${computerScore}`)
+}else if (!gameRound()) {
+    computerScore++
+    console.log(`Computer Score: ${computerScore}`)
+    console.log(`Player Score: ${playerScore}`)
+}else {
+    console.log(`Player Score: ${playerScore}`)
+    console.log(`Computer Score: ${computerScore}`)
+}
+}while (playerScore < 3 && computerScore < 3)
+
+if (playerScore > computerScore){
+    console.log("You won the game!!")
+}else{
+    console.log("You lost the game :(")
+}
+}
+Game();
